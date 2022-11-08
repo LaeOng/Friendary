e428bd3f0515ca2f467e4cc57134671b80a933ac
3217haeseong <32110495+3217haeseong@users.noreply.github.com>
2022-11-08 20:00:36 +0900

    Add files via upload

---
+++ pages/feed/invite.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style/invite.css">
+</head>
+<body>
+    <div id="header">
+        <img id="logo" src="../../asset/FriendaryLogo.png"/>
+    </div>
+    <div id="input_window">
+        <form class="window_form" id = "window_form" method = "POST">
+            <input type="text" id="email_input" name = "name" placeholder="이메일">
+            <button id = "submit_btn" type="submit">초대</button>
+            <button id = "cancel">취소</button>
+        </form>
+    </div>
+</body>
+</html>
